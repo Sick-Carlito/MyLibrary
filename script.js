c55070859025c17c682571560233a353e19298ce
Ben <benjisgarza@gmail.com>
2020-07-27 21:09:37 -0700

    Added local storage for persistent library
    Also cleaned up toggleFunction and multiple elements using the same ID

--- script.js
+++ script.js
@@ -1,4 +1,63 @@
-const myLibrary = [];
+let myLibrary = [];
+let storageAvailable = isStorageAvailable();
+let storagePopulated = isStoragePopulated();
+
+let data = getData('library');
+if(data){
+  for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+    myLibrary.push(element);
+  }
+  console.log(myLibrary,'getdata library');
+  render(myLibrary);
+}else{
+  console.log('no data');
+}
+
+function getData(key) {
+  if (isStorageAvailable() && isStoragePopulated(key)) {
+    let rawValue = window.localStorage.getItem(key);
+    console.log(rawValue,"rawValue")
+    let parsedValue = JSON.parse(rawValue);
+    console.log(parsedValue,"parsedValue")
+    return parsedValue;
+  }else{
+    console.log('local storage not utilized')
+    return false;
+  };
+};
+
+function setData(key,value) {
+  let valueString = JSON.stringify(value);
+  if (storageAvailable) {
+    window.localStorage.setItem(key,valueString);
+    console.log(key, valueString,'Item set');
+  };
+  console.log(value, valueString);
+};
+
+
+function isStorageAvailable() {
+  try {
+    let storage = window.localStorage;
+    let test = 'storage_test';
+    storage.setItem(test,test);
+    storage.removeItem(test);
+    return true;
+  }
+  catch(error) {
+    return error instanceof DOMException && (
+    error.code === 22 ||
+    error.code === 1014 ||
+    error.name === 'QuotaExceededError' ||
+    e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+    (storage && storage.length !== 0);
+  }
+};
+
+function isStoragePopulated(key) {
+  if (window.localStorage.getItem(key) !== null && !undefined){return true}
+};
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -14,11 +73,13 @@ Book.prototype.readToggle = function() {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+  setData('library',myLibrary);
 }
 
 function removeFromLibrary(index){
   myLibrary.splice(index,1);
-  console.log(index);
+  console.log(useLocalStorage, myLibrary);
+  setData('library',myLibrary);
 }
 
 
@@ -33,12 +94,8 @@ function render(library) {
               ${element.title},
               ${element.author},
               ${element.pages}
-
-              
-
               <button class='removeBtnn' id='btn-read-${index}' >${element.read? "Read" : "Not read"}</button>
               <button class='removeBtn' id='btn-remove-${index}' >Remove</button>
-
               </div>`
   divContainer.appendChild(card);
   console.log(myLibrary);
@@ -60,7 +117,7 @@ const titleText = document.querySelector('#input-title');
 const authorText = document.querySelector('#input-author');
 const pagesText = document.querySelector('#input-pages');
 const readText = document.querySelector('#input-read');
-const submitBook = document.querySelector('#btn-add'); //Changed to consistant id naming
+const submitBook = document.querySelector('#btn-add'); 
 
 
 function clearFields() {
@@ -71,15 +128,9 @@ function clearFields() {
 }
 
 function toggleFunction() {
-  var x = document.getElementById("input-form");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+  var inputForm = document.querySelector("#input-form");
+  inputForm.classList.toggle('block')  
 }
-
-
 
 submitBook.addEventListener('click', () => {
   const title = titleText.value;
